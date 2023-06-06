8f9e566a1b3ad2ce4bcd9f31b389bf2a6f3915aa
LeTuanVu-Dev <letuanvu425@gmail.com>
2023-06-06 20:21:55 +0700

    Initial commit

---
+++ mylibrary/src/main/java/com/mobiai/mylibrary/storage/StorageCommon.kt
@@ -0,0 +1,10 @@
+package com.mobiai.mylibrary.storage
+/*
+
+import androidx.lifecycle.MutableLiveData
+import com.ads.control.ads.wrapper.ApNativeAd
+
+class StorageCommon {
+    var nativeAdLanguage = MutableLiveData<ApNativeAd>()
+
+}*/
